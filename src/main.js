99484ccabdbbcfcc222cb11c939b959ff35dc4da
karlito40 <karlito40@gmail.com>
2023-02-21 11:49:36 +0100

    revert to the default status bar

--- src/main.js
+++ src/main.js
@@ -1,22 +1,22 @@
 import { createApp } from "vue";
-import { Capacitor } from "@capacitor/core";
-import { StatusBar, Style } from "@capacitor/status-bar";
+// import { Capacitor } from "@capacitor/core";
+// import { StatusBar, Style } from "@capacitor/status-bar";
 import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
 
-setupMobile();
+// setupMobile();
 
 const app = setupApp();
 app.mount("#app");
 
-function setupMobile() {
-  if (Capacitor.isPluginAvailable("StatusBar")) {
-    StatusBar.setOverlaysWebView({ overlay: true });
-    StatusBar.setStyle({ style: Style.Light });
-  }
-}
+// function setupMobile() {
+//   if (Capacitor.isPluginAvailable("StatusBar")) {
+//     StatusBar.setOverlaysWebView({ overlay: true });
+//     StatusBar.setStyle({ style: Style.Light });
+//   }
+// }
 
 function setupApp() {
   const app = createApp(App);
